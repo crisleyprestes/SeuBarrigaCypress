37bc27f5a24c7576a813e037a78d10fa1dcceb47
Crisley Linhares <prestes40@gmail.com>
2023-04-28 14:28:46 -0400

    Passando dados de json de movimentações para um arquivo fixture

--- cypress/e2e/interface/transactions.cy.js
+++ cypress/e2e/interface/transactions.cy.js
@@ -2,7 +2,6 @@
 
 import biuldEnv from "../../support/buildEnv"
 import locators from "../../support/locators"
-import moment from "moment"
 
 describe('Should test transactions feature at an interface level', () => {
 
@@ -27,15 +26,9 @@ describe('Should test transactions feature at an interface level', () => {
             valor: 123
         })
 
-        cy.intercept('GET', '/extrato/**', 
-        [
-        {conta: "Conta com movimentacao", id: 1610912, descricao: "Movimentacao de conta", envolvido: "BBB", observacao: null, tipo: "DESP", data_transacao: "2023-04-24T03:00:00.000Z", data_pagamento: "2023-04-24T03:00:00.000Z", valor: "-1500.00", status: true, conta_id: 1720266, usuario_id: 36898, transferencia_id: null, parcelamento_id: null},
-        {conta: "Conta para saldo", id: 1610913, descricao: "Movimentacao 1, calculo saldo", envolvido: "CCC", observacao: null, tipo: "REC", data_transacao: "2023-04-24T03:00:00.000Z", data_pagamento: "2023-04-24T03:00:00.000Z", valor: "3500.00", status: false, conta_id: 1720267, usuario_id: 36898, transferencia_id: null, parcelamento_id :null},
-        {conta: "Conta para saldo", id: 1610914, descricao: "Movimentacao 2, calculo saldo", envolvido: "DDD", observacao: null, tipo: "DESP", data_transacao: "2023-04-24T03:00:00.000Z", data_pagamento: "2023-04-24T03:00:00.000Z", valor: "-1000.00", status: true, conta_id: 1720267, usuario_id: 36898, transferencia_id: null, parcelamento_id: null},
-        {conta: "Conta para saldo", id: 1610915, descricao: "Movimentacao 3, calculo saldo", envolvido: "EEE", observacao: null, tipo: "REC", data_transacao: "2023-04-24T03:00:00.000Z", data_pagamento: "2023-04-24T03:00:00.000Z", valor: "1534.00", status: true, conta_id: 1720267, usuario_id: 36898, transferencia_id: null, parcelamento_id: null},
-        {conta: "Conta para extrato", id: 1610916, descricao: "Movimentacao para extrato", envolvido: "FFF", observacao: null, tipo: "DESP", data_transacao: "2023-04-24T03:00:00.000Z", data_pagamento: "2023-04-24T03:00:00.000Z", valor: "-220.00", status: true, conta_id: 1720268, usuario_id: 36898, transferencia_id: null, parcelamento_id: null},
-        {conta: "Conta para extrato", id: 31433, descricao: "Teste", envolvido: "Crisley", observacao: null, tipo: "REC", data_transacao: "2023-04-24T03:00:00.000Z", data_pagamento: "2023-04-24T03:00:00.000Z", valor: "123.00", status: true, conta_id: 1720268, usuario_id: 36898, transferencia_id: null, parcelamento_id: null}
-        ])
+        cy.fixture('movimentacaoSalva').then((movimentacaoSalva) => {
+            cy.intercept('GET', '/extrato/**', movimentacaoSalva)
+        })
 
         cy.createTransaction('Teste', '123', 'Crisley', 'Banco')
         cy.get(locators.MESSAGE).should('contain', 'Movimentação inserida com sucesso!')
