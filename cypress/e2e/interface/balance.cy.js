1771cd591aa74808856ee94f2d5c32cea05c99a5
Crisley Linhares <prestes40@gmail.com>
2023-04-28 15:29:52 -0400

    Obter e atualizar saldo com dados mockados

--- cypress/e2e/interface/balance.cy.js
+++ cypress/e2e/interface/balance.cy.js
@@ -1,27 +1,44 @@
 /// <reference types="cypress" />
 
 import locators from "../../support/locators"
+import biuldEnv from "../../support/buildEnv"
 
 describe('Should test balance feature at an interface level', () => {
 
+    afterEach(() => {
+        cy.clearLocalStorage()
+    })
+
     beforeEach(() => {
-        cy.login('crisley@mail.com', '123456')
-        cy.resetData()
+        biuldEnv()
+        cy.login('crisley@mail.com', 'senha errada')
     })
 
     it('Should get balance', () => {
         cy.get(locators.MENU.HOME).click()
-        cy.xpath(locators.HOME.SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+        cy.xpath(locators.HOME.SALDO_CONTA('Carteira')).should('contain', '100,00')
         cy.xpath(locators.HOME.SALDO_TOTAL)
-            .should('contain', '-')
-            .and('contain', '2.686,00')
+            .should('contain', '1.000.100,00')
     })
 
-    it('Should get balance updated', () => {
+    it.only('Should get balance updated', () => {
+        cy.fixture('movimentacaoParaSaldo').then((movimentacaoParaSaldo) => {
+            cy.intercept('GET', '/transacoes/**', movimentacaoParaSaldo)
+        })
+
+        cy.fixture('movimentacaoParaSaldo').then((movimentacaoParaSaldo) => {
+            cy.intercept('PUT', '/transacoes/**', movimentacaoParaSaldo)
+        })
+
         cy.editTransaction('Movimentacao 1, calculo saldo')
         cy.get(locators.MESSAGE).should('contain', 'Movimentação alterada com sucesso!')
+
+        cy.fixture('getSaldo').then((getSaldo) => {
+            cy.intercept('GET', '/saldo', getSaldo)
+        })
+
         cy.get(locators.MENU.HOME).click()
-        cy.xpath(locators.HOME.SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
-        cy.xpath(locators.HOME.SALDO_TOTAL).should('contain', '814,00')
+        cy.xpath(locators.HOME.SALDO_CONTA('Carteira')).should('contain', '4.034,00')
+        cy.xpath(locators.HOME.SALDO_TOTAL).should('contain', '1.004.034,00')
     })
 })
